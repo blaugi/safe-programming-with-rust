03346ea2c5349bca0fbcf9c7082d2b6b90172293
Matheus Girardi <mathe@BOOK-UJDPTQ4QBM.>
2025-02-27 21:33:09 -0300

    hw1 init

---
+++ hw1/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let q: i8 = 1;
+    let w: i16 = 2;
+    let e: i32 = 3;
+    let r: i64 = 4;
+    let t: i128 = 5;
+    let y: u8 = 6;
+    let u: u16 = 7;
+    let i: u32= 8;
+    let o: u64= 9;
+    let p: u128= 10;
+
+    println!("valor={}",q);
+    println!("valor={}",w);
+    println!("valor={}",e);
+    println!("valor={}",r);
+    println!("valor={}",t);
+    println!("valor={}",y);
+    println!("valor={}",u);
+    println!("valor={}",i);
+    println!("valor={}",o);
+    println!("valor={}",p);
+
+}
+ 
---
+++ hw1/target/debug/deps/hw1-43be50e16401f8b6.d
@@ -0,0 +1,5 @@
+/home/mathe/rust/hw1/target/debug/deps/libhw1-43be50e16401f8b6.rmeta: src/main.rs
+
+/home/mathe/rust/hw1/target/debug/deps/hw1-43be50e16401f8b6.d: src/main.rs
+
+src/main.rs:
